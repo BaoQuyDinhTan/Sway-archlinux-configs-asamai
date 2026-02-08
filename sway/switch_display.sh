225d0fa51583390357947477333fbc186f68afcd
Bảo Quý Định Tân <baoquydinhtanpt1@gmail.com>
2026-02-08 15:16:24 +0700

    Add files via upload

--- sway/switch_display.sh
+++ sway/switch_display.sh
@@ -1,35 +1,92 @@
 #!/bin/bash
 INTERNAL="eDP-1"
-EXTERNAL="HDMI-A-1"
 
-# 1. Define the options that will appear in the menu
-options="Laptop\nExternal\nBoth"
+# 1. Safer detection using jq (handles names better than grep)
+EXTERNAL=$(swaymsg -t get_outputs | jq -r '.[] | select(.name != "'$INTERNAL'").name' | head -n 1)
 
-# 2. Show the menu using Rofi (matches your system style)
-# It waits for you to pick an option
+if [ -z "$EXTERNAL" ]; then
+    notify-send "Display Error" "No external monitor found."
+    exit 1
+fi
+
+# 2. Main Menu
+options="Laptop Only\nExternal Only\nBoth (Extend)"
 selected=$(echo -e "$options" | rofi -dmenu -i -p "Display Mode")
 
-# 3. Check connection status for safety
-IS_CONNECTED=$(swaymsg -t get_outputs | grep "$EXTERNAL")
-
-# 4. Execute the command based on your choice
 case $selected in
-    "Laptop")
-        swaymsg output $EXTERNAL disable
-        swaymsg output $INTERNAL enable
+    "Laptop Only")
+        swaymsg output "$EXTERNAL" disable
+        swaymsg output "$INTERNAL" enable
         ;;
-    "External")
-        if [ -z "$IS_CONNECTED" ]; then
-            notify-send "Error" "External monitor not detected!"
-        else
-            swaymsg output $INTERNAL disable
-            swaymsg output $EXTERNAL enable
-        fi
+    "External Only")
+        swaymsg output "$INTERNAL" disable
+        swaymsg output "$EXTERNAL" enable
         ;;
-    "Both")
-        swaymsg output $INTERNAL enable
-        if [ -n "$IS_CONNECTED" ]; then
-            swaymsg output $EXTERNAL enable
-        fi
+    "Both (Extend)")
+        # Enable both to read resolutions
+        swaymsg output "$INTERNAL" enable
+        swaymsg output "$EXTERNAL" enable
+        sleep 1
+
+        # Get Dimensions
+        INT_W=$(swaymsg -t get_outputs | jq -r '.[] | select(.name=="'$INTERNAL'") | .rect.width')
+        INT_H=$(swaymsg -t get_outputs | jq -r '.[] | select(.name=="'$INTERNAL'") | .rect.height')
+        EXT_W=$(swaymsg -t get_outputs | jq -r '.[] | select(.name=="'$EXTERNAL'") | .rect.width')
+        EXT_H=$(swaymsg -t get_outputs | jq -r '.[] | select(.name=="'$EXTERNAL'") | .rect.height')
+
+        # Sub-menu
+        pos_options="Right of Laptop\nLeft of Laptop\nAbove Laptop\nBelow Laptop"
+        pos_selected=$(echo -e "$pos_options" | rofi -dmenu -i -p "Position of External?")
+
+        case $pos_selected in
+            "Right of Laptop")
+                # Center Vertically
+                DIFF_H=$(( ($EXT_H - $INT_H) / 2 ))
+                if [ $EXT_H -gt $INT_H ]; then
+                    swaymsg output "$INTERNAL" pos 0 "$DIFF_H"
+                    swaymsg output "$EXTERNAL" pos "$INT_W" 0
+                else
+                    DIFF_H=${DIFF_H#-} # Absolute value
+                    swaymsg output "$INTERNAL" pos 0 0
+                    swaymsg output "$EXTERNAL" pos "$INT_W" "$DIFF_H"
+                fi
+                ;;
+            "Left of Laptop")
+                # Center Vertically
+                DIFF_H=$(( ($EXT_H - $INT_H) / 2 ))
+                if [ $EXT_H -gt $INT_H ]; then
+                    swaymsg output "$EXTERNAL" pos 0 0
+                    swaymsg output "$INTERNAL" pos "$EXT_W" "$DIFF_H"
+                else
+                    DIFF_H=${DIFF_H#-}
+                    swaymsg output "$EXTERNAL" pos 0 "$DIFF_H"
+                    swaymsg output "$INTERNAL" pos "$EXT_W" 0
+                fi
+                ;;
+            "Above Laptop")
+                # Center Horizontally (Fixes the "weird" wall issue)
+                DIFF_W=$(( ($EXT_W - $INT_W) / 2 ))
+                if [ $EXT_W -gt $INT_W ]; then
+                    swaymsg output "$EXTERNAL" pos 0 0
+                    swaymsg output "$INTERNAL" pos "$DIFF_W" "$EXT_H"
+                else
+                    DIFF_W=${DIFF_W#-}
+                    swaymsg output "$EXTERNAL" pos "$DIFF_W" 0
+                    swaymsg output "$INTERNAL" pos 0 "$EXT_H"
+                fi
+                ;;
+            "Below Laptop")
+                # Center Horizontally
+                DIFF_W=$(( ($EXT_W - $INT_W) / 2 ))
+                if [ $EXT_W -gt $INT_W ]; then
+                    swaymsg output "$INTERNAL" pos "$DIFF_W" 0
+                    swaymsg output "$EXTERNAL" pos 0 "$INT_H"
+                else
+                    DIFF_W=${DIFF_W#-}
+                    swaymsg output "$INTERNAL" pos 0 0
+                    swaymsg output "$EXTERNAL" pos "$DIFF_W" "$INT_H"
+                fi
+                ;;
+        esac
         ;;
 esac
